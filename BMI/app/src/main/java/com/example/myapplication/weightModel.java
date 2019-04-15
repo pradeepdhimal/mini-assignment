87ff672cce1e4a827c60ed7d69419c50860a2b54
Pradeep <pradeepdhimal1994@gmail.com>
2019-04-15 12:23:21 +0545

    assignment completed on time

---
+++ BMI/app/src/main/java/com/example/myapplication/weightModel.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+
+
+public class weightModel {
+    private Float weight,height;
+
+    public weightModel(Float weight, Float height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public Float getHeight() {
+        return height;
+    }
+
+    public void setHeight(Float height) {
+        this.height = height;
+    }
+}
+
---
+++ DigitConverter/app/src/main/java/com/example/digitconverter/Model.java
@@ -0,0 +1,27 @@
+package com.example.digitconverter;
+
+public class Model {
+    private String[] ones = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+            "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public Model() {
+    }
+
+    public Model(String[] ones, String[] tens) {
+        this.ones = ones;
+        this.tens = tens;
+    }
+
+    public String[] getOnes() {
+        return ones;
+    }
+
+    public void setOnes(String[] ones) {
+        this.ones = ones;
+    }
+
+    public String[] getTens() {
+        return tens;
+    }
+}
